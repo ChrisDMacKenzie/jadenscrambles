8f1157a4f623dd7c460980a9ab85efb59b0f21e9
ChrisDMacKenzie <mackenziec92@gmail.com>
2015-10-08 20:51:54 -0400

    implemented tweet age validation

--- jaden.py
+++ jaden.py
@@ -1,6 +1,10 @@
 import twitter
 from random import shuffle
-import os
+from datetime import datetime  
+from sys import argv
+
+script, botpath = argv
+
 
 def main():
     myAcctData = getAcctData()
@@ -12,24 +16,37 @@ def main():
 
     Jaden = tweet.GetUserTimeline(262794965)
 
-    for status in Jaden:
-        words = status.text.split(' ')
-        words_to_use = [word for word in words if 'http' not in word]
-        shuffle(words_to_use)
-        new_status = ' '.join(words_to_use)
-        jadens_genius = tweet.PostMedia(new_status, 'http://i.imgur.com/y0GDcOv.png')
-        break
+    startTime = datetime.now()
+
+    for status in reversed(Jaden[0:10]):
+        
+        time = parseTime(status, startTime)
+        age = startTime - time
+        print time, age, startTime
+        if (age.days < 5):
+            words = status.text.split(' ')
+            words_to_use = [word for word in words if 'http' not in word]
+            shuffle(words_to_use)
+            new_status = ' '.join(words_to_use)
+            jadens_genius = tweet.PostMedia(new_status, 'http://i.imgur.com/y0GDcOv.png')
+        
 
 def getAcctData():
     # Get Oauth info from file
-    keyfile = os.environ.get('BOTPATH') + '/keyfile.txt'
+    keyfile = botpath + '/keyfile.txt'
     Odict = {}
     lines = [line.strip() for line in open(keyfile)]
     for line in lines:
         key, value = tuple(line.split('='))
         Odict[key] = value
-        print "%s: %s" % (key, value)
     return Odict
+
+
+def parseTime(tweet, startTime):
+    # Take status and return datetime object
+    strTime = ' '.join(
+        tweet.created_at.split(' ')[:4]) + ' ' + str(startTime.year)
+    return datetime.strptime(strTime, '%a %b %d %H:%M:%S %Y')
 
 main()
     
