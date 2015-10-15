04901d24207b299a3a60cc3b730995f8e63ac681
ChrisDMacKenzie <mackenziec92@gmail.com>
2015-10-14 20:07:01 -0400

    added logging

--- jaden.py
+++ jaden.py
@@ -23,13 +23,17 @@ def main():
         time = parseTime(status, startTime)
         age = startTime - time
         print time, age, startTime
-        if (age.days < 5):
+        new_tweets = 0
+        tweet_list = []
+        if (age.seconds < 3600):
             words = status.text.split(' ')
             words_to_use = [word for word in words if 'http' not in word]
             shuffle(words_to_use)
             new_status = ' '.join(words_to_use)
             jadens_genius = tweet.PostMedia(new_status, 'http://i.imgur.com/y0GDcOv.png')
-        
+            new_tweets += 1
+            tweet_list.append(jadens_genius.text)
+        LogTweets(startTime, new_tweets, tweet_list, time)
 
 def getAcctData():
     # Get Oauth info from file
@@ -47,6 +51,20 @@ def parseTime(tweet, startTime):
     strTime = ' '.join(
         tweet.created_at.split(' ')[:4]) + ' ' + str(startTime.year)
     return datetime.strptime(strTime, '%a %b %d %H:%M:%S %Y')
+
+def LogTweets(runTime, tweets, tweet_list, orig_tweet_time):
+    logfile = open(botpath + '/log.txt', 'w')
+    logfile.write("Running at: %s\n" % runTime)
+    logfile.write("Made %d new tweet(s):" % tweets)
+    for tweet in tweet_list:
+        tweet = tweet.encode('utf-8')
+        logfile.write("\n\n\t%s" % tweet)
+        logfile.write("\n\tOriginal status at %s\n\n" % orig_tweet_time)
+    logfile.close()
+
+                  
+
+
 
 main()
     
